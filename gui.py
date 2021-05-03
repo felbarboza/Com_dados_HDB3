f733540bf5ff091d1e83f8e020d47ee9947b1ef9
cheddargt <gus_ghz@live.com>
2021-05-02 23:29:58 -0300

    adicionado word wrap + textos e botoes

--- gui.py
+++ gui.py
@@ -59,13 +59,16 @@ def gerarGraficoRecebido():
 def enviar():
     print()
 
+def iniciarServidor():
+    print()
+
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
 canvas.pack()
 
 frame = tk.Frame(root, bg='#282828')
 frame.place(relx = 0.005, rely = 0.01, relwidth=0.99, relheight=0.98)
 
-label_ins_msg = tk.Label(frame, text="Insira a mensagem")
+label_ins_msg = tk.Label(frame, text="Insira a mensagem", wraplength=200)
 label_ins_msg.grid(row=0, column=0)
 
 entry_msg = tk.Entry(frame, bd = 5, textvariable = msg_codificada)
@@ -77,21 +80,21 @@ btn_criptografar.grid(row=1, column=1)
 label_cript = tk.Label(frame, text="Mensagem criptografada:")
 label_cript.grid(row=2, column=0)
 
-label_msg_criptografada = tk.Label(frame, text= msg_codificada.get())
+label_msg_criptografada = tk.Label(frame, text= msg_codificada.get(), wraplength=200)
 label_msg_criptografada.config(text="<aguardando criptografia>")
 label_msg_criptografada.grid(row=3, column=0)
 
 btn_binarizar = tk.Button(frame, text="Transformar em binário", command = binarizarMensagem)
 btn_binarizar.grid(row=3, column=1)
 
-label_msg_binarizada = tk.Label(frame, text= msg_codificada.get())
+label_msg_binarizada = tk.Label(frame, text= msg_codificada.get(), wraplength=200)
 label_msg_binarizada.config(text="<aguardando binarização>")
 label_msg_binarizada.grid(row=4, column=0)
 
 btn_aplicar_codificacao = tk.Button(frame, text="Aplicar codificação", command = aplicarCodificacao)
 btn_aplicar_codificacao.grid(row=4, column=1)
 
-label_msg_aplicar_codificacao = tk.Label(frame, text= msg_codificada.get())
+label_msg_aplicar_codificacao = tk.Label(frame, text= msg_codificada.get(), wraplength=200)
 label_msg_aplicar_codificacao.config(text="<aguardando codificação>")
 label_msg_aplicar_codificacao.grid(row=5, column=0)
 
@@ -108,17 +111,53 @@ frame_separate.grid(row=0, column=2)
 frame_separate = tk.Frame(frame, bg='#282828', width=50, height=20)
 frame_separate.grid(row=1, column=2)
 
-# Receptor
+#### Receptor ####
 
-label_recebida = tk.Label(frame, text= "Mensagem recebida:")
-label_recebida.grid(row=0, column=3)
+btn_iniciarServidor = tk.Button(frame, text="Iniciar servidor", command = iniciarServidor)
+btn_iniciarServidor.grid(row=0, column=3)
 
-label_msg_recebida = tk.Label(frame, text= "")
-label_msg_recebida.config(text="<aguardando receber mensagem>")
-label_msg_recebida.grid(row=1, column=3)
+# Recepção
+
+label_recebida_codificada = tk.Label(frame, text= "Mensagem recebida:")
+label_recebida_codificada.grid(row=1, column=3)
+
+label_msg_recebida_codificada = tk.Label(frame, text= "", wraplength=200)
+label_msg_recebida_codificada.config(text="<aguardando receber mensagem>")
+label_msg_recebida_codificada.grid(row=2, column=3)
+
+# Apresentar o gráfico
 
 btn_gerar_grafico_recebido = tk.Button(frame, text="Gerar gráfico", command = gerarGraficoRecebido)
-btn_gerar_grafico_recebido.grid(row=2, column=3)
+btn_gerar_grafico_recebido.grid(row=3, column=3)
+
+# Aplicar o algoritmo de codificação de linha de modo inverso
+
+label_recebida_decodificada = tk.Label(frame, text= "Mensagem decodificada:")
+label_recebida_decodificada.grid(row=4, column=3)
+
+label_msg_recebida_decodificada = tk.Label(frame, text= "", wraplength=200)
+label_msg_recebida_decodificada.config(text="<aguardando decodificação>")
+label_msg_recebida_decodificada.grid(row=5, column=3)
+
+# Transformação de binário para ASCII
+
+label_recebida_desbinarizada = tk.Label(frame, text= "Mensagem em binário -> ASCII:")
+label_recebida_desbinarizada.grid(row=6, column=3)
+
+label_msg_recebida_desbinarizada = tk.Label(frame, text= "", wraplength=200)
+label_msg_recebida_desbinarizada.config(text="<aguardando conversão>")
+label_msg_recebida_desbinarizada.grid(row=7, column=3)
+
+# Aplicar o algoritmo de criptografia inverso
+
+label_recebida_decriptografada = tk.Label(frame, text= "Mensagem decriptografada:")
+label_recebida_decriptografada.grid(row=8, column=3)
+
+# Mostrar a mensagem
+
+label_msg_recebida_decriptografada = tk.Label(frame, text= "", wraplength=200)
+label_msg_recebida_decriptografada.config(text="<aguardando decriptografia>")
+label_msg_recebida_decriptografada.grid(row=9, column=3)
 
 
 root.mainloop()
